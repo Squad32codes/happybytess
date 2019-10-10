7b3e9c929b4b0cf680856d02c3e7627a847a5f7c
Squad32Codes <Squad32Codes@gmail.com>
2019-10-10 16:10:05 -0500

    added slider bars to surveyComplex for Cody review

--- app/src/main/assets/js/surveyComplex.js
+++ app/src/main/assets/js/surveyComplex.js
@@ -1,9 +1,27 @@
+   // A $( document ).ready() block.
+var slideA = 0;
+var slideB = 0;
+var slideC = 0;
 
-
-    // A $( document ).ready() block.
     $( document ).ready(function() {
+
      $('#conditionSlider').change(function(){
             $('#sliderValue').html($('#conditionSlider').val());
+            slideA = $('#conditionSlider').val();
+            return(slideA);
+     });
 
-      });
-    });
+     $('#conditionSlider2').change(function(){
+            $('#sliderValue2').html($('#conditionSlider2').val());
+            slideB = $('#conditionSlider2').val();
+            return(slideB);
+     });
+
+     $('#conditionSlider3').change(function(){
+            $('#sliderValue3').html($('#conditionSlider3').val());
+            slideC = $('#conditionSlider3').val();
+            return(slideC);
+     });
+
+
+   });
